2a084a0bca0441f5a832992ea0ba983b853ff76d
Vinaypartap Singh <developervsandhu@gmail.com>
2025-04-23 09:02:17 -0700

    Swagger Docs Integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Nest Js Documentation Using Swagger Module")
+    .setDescription("This is an documentation about nest js api module I am still Learning and testing new modules")
+    .setTermsOfService("http://localhost:3000/terms")
+    .setLicense('MIT', 'https://github.com/vinaypartap-singh')
+    .setContact('Email', 'developervsandhu@gmail.com', 'developervsandhu@gmail.com')
+    .addServer("https://localhost:3000")
+    .setVersion('1').build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document)
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,47 @@
 import { Body, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { UserService } from './providers/users.service';
 
+@ApiTags("Users")
 @Controller('users')
 export class UsersController {
 
-    constructor(
-        private readonly usersService: UserService
-    ) {
-
-    }
-
+    constructor(private readonly usersService: UserService) { }
 
     @Get()
+    @ApiOperation({
+        summary: "Get All User Here"
+    })
     public getUsers() {
         return this.usersService.findAll()
     }
-
     @Get("/:id/")
-    public getNewUser(@Param('id', ParseIntPipe) id: number, @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number) {
+    @ApiOperation({
+        summary: "Get Specific User"
+    })
+    @ApiParam({
+        name: "id",
+        type: "number",
+        required: false,
+        description: "User Id to Get Information of sepcific user",
+        example: 109812
+    })
+    @ApiQuery({
+        name: "limit",
+        type: "number",
+        required: false,
+        description: "The number of enteries returned per request",
+        example: 10
+    })
+    public getNewUser(@Param('id', ParseIntPipe) id: number, @Query('limit', new DefaultValuePipe(10), ParseIntPipe)
+    limit: number) {
         return this.usersService.findOnyById(id)
     }
 
     @Post()
+    @ApiOperation({
+        summary: "Add Users To Website"
+    })
     public postUser(@Body() req: any) {
         console.log(req)
         return "You send the post request to the user route"
